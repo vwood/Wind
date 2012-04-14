ceb7a05674a550131f856e0ebae76d92493b8202
VWood <vwood.org@gmail.com>
2012-04-15 00:20:59 +1200

    Fix button's size.

--- button.py
+++ button.py
@@ -7,20 +7,19 @@ from util import *
 class Button(widget.Widget):
     """A button that uses a callback."""
     def __init__(self, label="", callback = lambda: None, **kwargs):
-        super(Button, self).__init__(kwargs)
+        super(Button, self).__init__(**kwargs)
 
-        self.width, self.height = self.font.size(label)
+        w, h = self.font.size(label)
         self.padding = 4
-        self.width += self.padding * 2
-        self.height += self.padding * 2
+        self.resize(w + self.padding * 2,h + self.padding * 2)
+
         self.back_color = (120, 120, 200)
         self.label = label
         self.callback = callback
 
-    def display(self, surface, x=0, y=0, w=None, h=None):
+    def display(self, surface):
         """Display the selected widget, and the tabs."""
-        if w == None: w = surface.get_width() - x
-        if h == None: h = surface.get_height() - y
+        x, y, w, h = self.pos
         
         pygame.draw.rect(surface, self.back_color, Rect(x, y, w - 1, h - 1), 2)
         blit_text(surface, self.label, x + self.padding, y + self.padding, self.font, self.color)
