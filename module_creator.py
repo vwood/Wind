06049fe30ca64e71a4b9aec4cdbdad6e470d5f42
VWood <vwood.org@gmail.com>
2012-04-18 21:20:55 +1200

    Module Creator is object.

--- module_creator.py
+++ module_creator.py
@@ -2,24 +2,15 @@ import sys
 import imp
 
 class Module_Creator(object):
+    count = 0
     
-my_code = 'a = 5'
+    def __init__(self, name, code):
+        self.module = imp.new_module('custom_module_' + str(Module_Creator.count))
+        Module_Creator.count += 1
+        self.blank_module_dict = self.module.__dict__.copy()
+        exec code in my_module.__dict__
 
-# Do we really need to name this?
-my_module = imp.new_module('my_module')
-blank_module_dict = my_module.__dict__.copy()
-
-exec my_code in my_module.__dict__
-
-print my_module.a
-
-for k,v in my_module.__dict__.items():
-    if blank_module_dict.has_key(k):
-        my_module.__dict__.pop(k)
-
-exec my_code in my_module.__dict__
-
-print my_module.a
-
-## to prevent re-importing
-# sys.modules['my_module'] = my_module
+    def remove_module(self):
+        for k,v in my_module.__dict__.items():
+            if blank_module_dict.has_key(k):
+                my_module.__dict__.pop(k)
